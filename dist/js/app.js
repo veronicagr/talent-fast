bf4e6a8f294161cb08894240f2db98ba45ba4a83
amcravila <amcravila@gmail.com>
2018-11-08 20:01:12 -0200

    cpfRequest

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,7 +6,6 @@ const numberCPF = $('#cpf');
 //   return cpfValidate(numberCPF);
 // }
 
-<<<<<<< HEAD
 function cpfRequest() {
   return fetch('https://talent-fest-e8129.firebaseio.com/consultaCPF.json')
     .then(response => response.json())
@@ -18,7 +17,8 @@ function loadCPF(data) {
   $.each(data, function(index, value) {
 
   });
-=======
+}
+
 // function cpfRequest() {
 //   const url = 'https://raw.githubusercontent.com/adrianosferreira/afrodite.json/master/afrodite.json';
 //
@@ -39,14 +39,12 @@ function loadCPF(data) {
 //   });
 // }
 
-function formatar(mascara, documento){
-  var i = documento.value.length;
-  var saida = mascara.substring(0,1);
-  var texto = mascara.substring(i);
+function formatar(mascara, documento) {
+  let i = documento.value.length;
+  let saida = mascara.substring(0,1);
+  let texto = mascara.substring(i);
 
-  if (texto.substring(0,1) != saida){
-            documento.value += texto.substring(0,1);
+  if (texto.substring(0,1) != saida) {
+    documento.value += texto.substring(0,1);
   }
-
->>>>>>> a54435da95921f8134a4385dd6731cd8ec0a8a76
 }
