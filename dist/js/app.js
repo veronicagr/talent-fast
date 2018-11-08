0a3e84a0c4155150bc29ba1cdf9cfa00ec604e37
amcravila <amcravila@gmail.com>
2018-11-08 15:31:23 -0200

    Criada pasta DIST

---
+++ dist/js/app.js
@@ -0,0 +1,25 @@
+const cpfValidate = require('validar-cpf');
+
+function validator(numberCPF) {
+  return cpfValidate(numberCPF);
+}
+
+// function cpfRequest() {
+//   const url = 'https://raw.githubusercontent.com/adrianosferreira/afrodite.json/master/afrodite.json';
+//
+//   $.ajax({
+//     type: 'GET',
+//     dataType: 'json',
+//     url,
+//     success: loadCPF,
+//     error
+//   });
+// }
+//
+// function loadCPF(data) {
+//   $.each(data, function(index, value) {
+//     nameRecipe = data[index].nome;
+//     idRecipe = data[index]._id['$oid'];
+//     showAllRecipes();
+//   });
+// }
---
+++ dist/js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(() => {
+  page('/', index);
+  page('/newuser', newuser);
+  page('/newuser/myinformations', myInformations);
+  page('/newuser/myadress', myAdress);
+  page('/newuser/docvalidation', docValidation);
+  page('/newuser/limit', limitAccount);
+  page('/login', loginUser);
+  page('/employees', loginEmployees);
+  page('/employees/pending', pendingClient);
+  page('/employees/approved', approvedClient);
+});
+page();
+
+function index() {
+  //vai retornar um bem-vindo!
+}
+
+function newuser() {
+  //o input de nome e cpf
+}
+
+function myInformations() {
+  //mais dados são requisitados do usuario, como nascimento, estado civil...
+}
+
+function myAdress() {
+  //input do endereço
+}
+
+function docValidation() {
+  //validação do documento com foto
+}
+
+function limitAccount() {
+  //retorna o limite aprovado
+}
+
+function loginUser() {
+  //retorna uma pagina para os clientes entrarem
+}
+
+function loginEmployees() {
+  //pagina de login de acesso ao gerente
+}
+
+function pendingClient() {
+  //depois de ja logado a pagina exibe os clientes que estão pendentes para a aprovação
+}
+
+function approvedClient() {
+  //retorna os clientes já aprovados pelo gerente
+}
