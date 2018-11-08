65e999dd4c729f8101f0e1aca22e7a8de3bd546a
amcravila <amcravila@gmail.com>
2018-11-08 21:43:13 -0200

    merge branch Amanda

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,9 +2,9 @@
 const cpfValidate = require('validar-cpf');
 const numberCPF = $('#cpf');
 
-// function validator(numberCPF) {
-//   return cpfValidate(numberCPF);
-// }
+function validator(numberCPF) {
+  return cpfValidate(numberCPF);
+}
 
 function cpfRequest() {
   return fetch('https://talent-fest-e8129.firebaseio.com/consultaCPF.json')
