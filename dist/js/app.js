e8cd2df846ad0ca99425745604cd0446787e5676
amcravila <amcravila@gmail.com>
2018-11-09 02:40:12 -0200

    merge app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-const validarCpf = require('validar-cpf');
-
-<<<<<<< HEAD
-const cpfValidate = require('validar-cpf');
-const numberCPF = $('#cpf');
-
-function validator(numberCPF) {
-  return cpfValidate(numberCPF);
-=======
-function verifyCPF() {
-  let numberCPF = $('#cpf').val();
-  console.log(numberCPF);
-  console.log(validarCpf(numberCPF));
->>>>>>> cd41a1c3876eab7036aa84652163df8751bef448
-=======
 function verifyCPF() {
   let numberCPF = $('#cpf').val();
   let status = validarCpf(numberCPF);
-  if(status == false){
+  if (status === false) {
     $('#cpf').before('<p style="color:red">CPF inválido</p>');
-  }else{
-    page.redirect('/newuser/myinformations')
+  } else {
+    page.redirect('/newuser/myinformations');
   }
 }
 const validarCpf = input => {
@@ -28,11 +12,11 @@ const validarCpf = input => {
   if (cpf === '' || cpf.length !== 11 || !/^\d{11}$/.test(cpf)) {
   	return false;
   }
-  const digits = cpf.split('').map(x => parseInt(x))
+  const digits = cpf.split('').map(x => parseInt(x));
   for (let j = 0; j < 2; j++) {
     	let sum = 0;
     	for (let i = 0; i < 9 + j; i++) {
-    		sum += digits[i] * (10 + j - i)
+    		sum += digits[i] * (10 + j - i);
     }
     let checkDigit = 11 - sum % 11;
     if (checkDigit === 10 || checkDigit === 11) {
@@ -43,9 +27,7 @@ const validarCpf = input => {
     }
   }
   return true;
->>>>>>> 396e9f720ceebcd0a3f0c655c1e42982a360b18e
-}
-
+};
 
 function cpfRequest() {
   return fetch('https://talent-fest-e8129.firebaseio.com/consultaCPF.json')
@@ -81,7 +63,7 @@ function loadCPF(data) {
 //   });
 // }
 
-function format(mask, doc){
+function format(mask, doc) {
   let i = doc.value.length;
   let out = mask.substring(0,1);
   let txt = mask.substring(i);
